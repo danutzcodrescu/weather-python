0f5132dd0b95e3568007f21beefb90612e5763d5
danutzcodrescu <danutzcodrescu@gmail.com>
2018-05-22 23:17:03 +0200

    added try catch blocks and split in multiple functions

--- weather.call.py
+++ weather.call.py
@@ -5,28 +5,38 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-def weather(address):
+def getLocation(address):
   url = f"https://maps.googleapis.com/maps/api/geocode/json?key={os.environ['GMAPS_API_KEY']}&address={address}"
+  try:
+    _, content = httplib2.Http().request(url)
 
-  _, content = httplib2.Http().request(url)
+    json_data = json.loads(content)
+    return json_data['results'][0]['geometry']['location']
+  except httplib2.ServerNotFoundError:
+    print('error in gmaps')
 
-  json_data = json.loads(content)
-  location = json_data['results'][0]['geometry']['location']
-
+def getWeatherData(location): 
   url = f"https://api.darksky.net/forecast/{os.environ['DARK_SKY_API_KEY']}/{str(location['lat'])},{str(location['lng'])}?units=si&exclude=flags,alerts"
+  try:
+    _, content = httplib2.Http().request(url)
 
-  _, content = httplib2.Http().request(url)
+    return json.loads(content)
+  except httplib2.ServerNotFoundError:
+    print('error in darksky')
 
-  json_data = json.loads(content)
+def weather(address):
+  
+  location = getLocation(address)
+  weatherData = getWeatherData(location)
 
   time = datetime.datetime.fromtimestamp(
-      json_data['currently']['time']).strftime('%Y-%m-%d %H:%M:%S')
+      weatherData['currently']['time']).strftime('%Y-%m-%d %H:%M:%S')
 
-  currently = {'Time': time, 'Temperature': json_data['currently']
-              ['temperature'], 'Wind': json_data['currently']['windSpeed'], 'PrecipitationProbability': json_data['currently']['precipProbability']}
+  currently = {'Time': time, 'Temperature': weatherData['currently']
+              ['temperature'], 'Wind': weatherData['currently']['windSpeed'], 'PrecipitationProbability': weatherData['currently']['precipProbability']}
 
-  hourDict = {'Summary': json_data['hourly']['summary']}
-  for hour in json_data['hourly']['data']:
+  hourDict = {'Summary': weatherData['hourly']['summary']}
+  for hour in weatherData['hourly']['data']:
       time = datetime.datetime.fromtimestamp(
           hour['time']).strftime('%Y-%m-%d %H:%M')
       hourData = {'Time': time, 'Temperature': hour
